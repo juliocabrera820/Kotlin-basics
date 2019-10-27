4703d1da977a8a20a7f5c645ff42dc27d115b569
juliocabrera <juliocabrera820@gmail.com>
2019-10-26 19:41:07 -0500

    Variables

---
+++ src/Variables/Variables.kt
@@ -0,0 +1,12 @@
+package Variables
+
+fun main() {
+    val name = "val es inmutable"
+    println("Val $name")
+    var band = "in"
+    band = "in flames"
+    println("var $band")
+    var value = true
+    println(value)
+
+}
