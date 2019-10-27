4b1b4749fcae2b16ad2cd3dce57084b3d13c773a
juliocabrera <juliocabrera820@gmail.com>
2019-10-26 21:54:07 -0500

    Map

---
+++ src/Map/Map.kt
@@ -0,0 +1,13 @@
+package Map
+
+fun main() {
+
+    var langs:MutableMap<String,String> = mutableMapOf()
+    langs.put("Java",".java")
+    langs.put("GO",".go")
+    langs.put("Ruby","rb")
+    langs.put("Kotlin",".kt")
+    langs.put("Python",".py")
+    langs.put("Javascript",".js")
+    langs.forEach(({(k,v)-> println("Clave $k valor $v")}))
+}
